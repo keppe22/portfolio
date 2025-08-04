4ad322ca8419c21a972f7ab7857634eb92216be5
John Keppe <jhn.keppe@outlook.com>
2025-08-04 09:49:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+console.log("Boot screen loaded.");
+
 setTimeout(() => {
   document.getElementById("bootScreen").style.display = "none";
   document.getElementById("desktop").style.display = "block";
 }, 4000); // 4-second boot screen
+
+console.log("Switching to desktop view.");
